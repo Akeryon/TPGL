b1d27e6824ac963e7cfd7cf7cce3b23908ceef34
jm602808 <nathan8912@hotmail.fr>
2019-10-02 18:40:00 +0200

    Changement de structure (NetBeans)

---
+++ Panier/src/main/java/fr/ufrsciencestech/panier/Orange.java
@@ -0,0 +1,38 @@
+public class Orange
+{
+	double prix;
+	String origine;
+
+	public Orange(double p, String o)
+	{
+		
+		if(p<0)
+		this.prix=-p;
+		else
+		this.prix =p;
+		this.origine = o;
+	
+	}
+
+	public double getPrix()
+	{
+		return this.prix;
+	}
+	public String getOrigine()
+	{
+		return this.origine;
+	}
+	
+	public String toString()
+	{
+		return "Ceci est une orange au prix de "+getPrix()+" D'origine : "+ getOrigine();	
+	}
+	public static void main (String[] args)
+{
+	Orange o= new Orange(-4,"France");
+	System.out.println(o.toString());
+	
+
+}
+}
+
