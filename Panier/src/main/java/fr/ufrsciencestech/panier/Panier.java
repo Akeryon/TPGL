828e9767917d15214ab87c1dd5326d1183153762
jm602808 <nathan8912@hotmail.fr>
2019-10-02 19:13:33 +0200

    Changement de structure (NetBeans)

--- Panier/src/main/java/fr/ufrsciencestech/panier/Panier.java
+++ Panier/src/main/java/fr/ufrsciencestech/panier/Panier.java
@@ -71,7 +71,25 @@ public class Panier
 		}
 		return s;
 	}
-	
+        
+        public boolean equals(Panier p)
+        {
+            if (p.GetPrix()==GetPrix()&&p.GetNbOrange()==GetNbOrange())
+            {
+              // Collections.sort(LesOranges);
+               for (int i =0; i < N ; i++)
+               {
+                   if(LesOranges.get(i)!=p.LesOranges.get(i))
+                   {
+                       return false;
+                   }
+                   
+               }
+               return true;
+            }
+            return false;
+        }
+        
 	public String ToString()
 	{
 		String s =" Le panier contient " + GetNbOrange() +" Orange(s) \n";
@@ -83,21 +101,62 @@ public class Panier
 		s+= "Le prix du panier total est de : " +GetPrix();
 		return s;
 	}
+        public void boycotteOrigine(String s)
+        {
+            for(int i =0; i<N; i++)
+		{
+			if (LesOranges.get(i).getOrigine().equals(s))
+                        {
+                            LesOranges.remove(i--);
+                            N--;
+                        }
+		}
+            
+            
+        }
 
 public static void main (String[] args)
 {
 	Panier p = new Panier(5);
+        Panier p2 = new Panier(5);
+        Panier p3 = new Panier(5);
 	Orange o = new Orange(4,"France");
 	Orange o1 = new Orange(3.75,"Espagne");
+        Orange o3 = new Orange(4,"France");
 	Orange o2 = new Orange(0.49,"Maroc");
+        Orange o4 = new Orange(4,"France");
 	p.ajoute(o);
 	p.ajoute(o1);
 	p.ajoute(o2);
+        p.ajoute(o3);
+        p.ajoute(o4);
+        p2.ajoute(o);
+	p2.ajoute(o1);
+	p2.ajoute(o2);
+        p2.ajoute(o3);
+        p2.ajoute(o4);
+        p3.ajoute(o);
+	p3.ajoute(o1);
+	p3.ajoute(o2);
+        p3.ajoute(o3);
+        p3.ajoute(o4);
 	System.out.println(p.ToString());
 	
 	p.retire();
 	System.out.println(p.ToString());
 	p.ajoute(o2);
-System.out.println(p.ToString());
+        System.out.println(p.ToString());
+        p.boycotteOrigine("France");
+        System.out.println(p.ToString());
+        if (p.equals(p2))
+        {
+            System.out.println("Les deux paniers sont équivalents");
+            
+        }
+         if (p3.equals(p2))
+        {
+            System.out.println("Les deux paniers sont équivalents");
+            
+        }
 }
 }
